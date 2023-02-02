9c75a96740fc3e5272cfe96c8691c3fe26e6add0
Nick <gri.nik.v@yandex.ru>
2023-02-03 01:21:16 +0300

    Finish 019

--- Exercise019/Program.cs
+++ Exercise019/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Clear ();
+
+Console.Write ("Введите 5-ти значное число: ");
+string number = Console.ReadLine();
+int range = number.Length;
+
+if (range == 5)
+{
+    if (number[0] == number [4] && number [1] == number [3])
+    {
+        Console.WriteLine ($"Да, {number} это палиндром");
+    }
+    else 
+    {
+        Console.WriteLine ($"Нет, {number} это не палиндром!");
+    }
+}
+else
+{
+    Console.WriteLine ("Отставить! Вы ввели не 5-ти значное число!");
+}
